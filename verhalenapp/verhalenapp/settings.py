7ad9473d955c3c9aa07297f5e6e619a2af297323
Viniciuseduardosousa <33297@ma-web.nl>
2025-04-24 11:49:49 +0200

    settings edit

--- verhalenapp/verhalenapp/settings.py
+++ verhalenapp/verhalenapp/settings.py
@@ -40,8 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
-    'rest_framework.authtoken',
-    'auth_jwt',
+
 
     'rest_framework',
     'verhalen',
