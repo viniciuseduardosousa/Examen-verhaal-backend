787680c5c10cac9516e7b9f4971336a08197681d
Viniciuseduardosousa <33297@ma-web.nl>
2025-04-24 11:15:34 +0200

    live setup

--- verhalenapp/verhalenapp/settings.py
+++ verhalenapp/verhalenapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1((!gqn@19++lc#ga_mcn@3#_a_z-qwa&k$6y+k4zz!$+9m36z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vinininja123.pythonanywhere.com']
 
 
 # Application definition
