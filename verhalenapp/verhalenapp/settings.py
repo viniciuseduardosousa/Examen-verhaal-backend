34c7b2aac49f6dd2a13d725f481d94155f7279b0
Viniciuseduardosousa <33297@ma-web.nl>
2025-04-24 11:03:43 +0200

    static

--- verhalenapp/verhalenapp/settings.py
+++ verhalenapp/verhalenapp/settings.py
@@ -139,7 +139,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Define the base directory for static files
+STATIC_URL = '/static/'
+
+# Define locations where Django will look for static files
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# Define the directory where collectstatic will collect files for production
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
