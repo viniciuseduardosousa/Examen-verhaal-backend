aeb54bd7ea0857081dc07a5319b96047875dc757
Viniciuseduardosousa <33297@ma-web.nl>
2025-05-19 13:06:32 +0200

    libreoffice toegevoegd

--- verhalenapp/verhalen/models.py
+++ verhalenapp/verhalen/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import os
+import subprocess
+from django.core.files import File
 
 # class User(models.Model):
 #     naam = models.CharField(max_length=200)
@@ -27,10 +30,38 @@ class Verhaal(models.Model):
     is_uitgelicht = models.BooleanField(default=False)
     is_spotlighted = models.BooleanField(default=False)
     is_downloadable = models.BooleanField(default=False)
-    word_file = models.FileField(upload_to='verhalen_pdfs/', null=True, blank=True)
+    word_file = models.FileField(upload_to='verhalen_pdfs/docx/', null=True, blank=True)
+    pdf_file = models.FileField(upload_to='verhalen_pdfs/pdf/', null=True, blank=True)
 
 
     def __str__(self):
         return self.titel
 
+    def convert_to_pdf(self):
+        if self.word_file:
+            # Absolute path to the docx
+            docx_path = self.word_file.path
+            output_dir = os.path.dirname(docx_path)
+
+            # Convert with LibreOffice
+            subprocess.run([
+                '/opt/homebrew/bin/soffice',
+                '--headless',
+                '--convert-to', 'pdf',
+                '--outdir', output_dir,
+                docx_path
+            ], check=True)
+
+            # Construct expected output PDF file path
+            pdf_file = os.path.splitext(os.path.basename(docx_path))[0] + '.pdf'
+            pdf_path = os.path.join(output_dir, pdf_file)
+
+            # Save to FileField
+            with open(pdf_path, 'rb') as f:
+                self.pdf_file.save(pdf_file, File(f), save=False)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)  # Save initial file first
+        self.convert_to_pdf()         # Convert after saving
+        super().save(update_fields=['pdf_file'])  # Save only pdf field
 
