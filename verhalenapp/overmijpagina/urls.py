4ca9af43b36273adf87e6cb27fdd66819ef9e065
Viniciuseduardosousa <33297@ma-web.nl>
2025-05-23 22:26:09 +0200

    overmij en footer data; nu kunnen put en patch

--- verhalenapp/overmijpagina/urls.py
+++ verhalenapp/overmijpagina/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('overmij/', Overmijview.as_view(), name='over-mij'),
     path('footer/', Footerview.as_view(), name='footer'),
-
+    path('overmij/admin/', OvermijAdminview.as_view(), name='over-mij-admin'),
+    path('footer/admin/', FooterAdminview.as_view(), name='footer-admin'),
 ]
--- verhalenapp/overmijpagina/views.py
+++ verhalenapp/overmijpagina/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from .serializers import *
-
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -19,8 +19,42 @@ class Overmijview(APIView):
         return Response(serializer.data)
 
 class Footerview(APIView):
-
     def get(self,request):
         footer = Footer.objects.get()
         serializer = FooterSerializer(footer, many=False, context={'request': request})
         return Response(serializer.data)
+
+class OvermijAdminview(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        overmij = Overmij.objects.get()
+        serializer = OvermijSerializer(overmij, many=False, context={'request': request})
+        return Response(serializer.data)
+    def put(self, request):
+        overmij = Overmij.objects.get()
+        serializer = OvermijSerializer(overmij, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    def patch(self, request):
+        overmij = Overmij.objects.get()
+        serializer = OvermijSerializer(overmij, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    
+class FooterAdminview(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        footer = Footer.objects.get()
+        serializer = FooterSerializer(footer, many=False, context={'request': request})
+        return Response(serializer.data)
+    def put(self, request):
+        footer = Footer.objects.get()
+        serializer = FooterSerializer(footer, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
