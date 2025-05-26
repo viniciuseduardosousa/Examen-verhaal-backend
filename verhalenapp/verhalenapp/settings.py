b6a5fd185e246cb0eccaef117f642be251b2c7ef
Viniciuseduardosousa <33297@ma-web.nl>
2025-05-26 13:01:39 +0200

    jwt token lifetime change

--- verhalenapp/verhalenapp/settings.py
+++ verhalenapp/verhalenapp/settings.py
@@ -126,7 +126,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=3),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
